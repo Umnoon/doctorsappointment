94f3a8ef7d97ba3e3023aed4aaf61782c6560b20
Umnoon71 <44784594+Umnoon71@users.noreply.github.com>
2019-09-06 01:09:01 +0600

    Create dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUserName = "root";
+$dbPassword = "";
+$dbName = "doctorappointment";
+
+$connt = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+
+ 
+?>
